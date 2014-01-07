c7c774c49fa56f16f97c4ef025ed0ea22db24bbf
Victor Rocha <victor@rochapps.com>
2014-01-06 22:44:42 -0500

    Initializes ultrasonic sensor.

--- self-driving.py
+++ self-driving.py
@@ -6,33 +6,36 @@ from BrickPi import *
 class SelfDrivingCar(object):
 
     def __init__(self, speed):
-        self.speed = speed
+        self.speed = -speed
         self.setup()
 
     def setup(self):
         BrickPiSetup()  # setup the serial port for communication
-        BrickPi.MotorEnable[PORT_A] = 1 # Enable the Motor A
-        BrickPi.MotorEnable[PORT_B] = 1 # Enable the Motor B
+        BrickPi.MotorEnable[PORT_A] = 1  # Enable the Motor A
+        BrickPi.MotorEnable[PORT_B] = 1  # Enable the Motor B
+        BrickPi.SensorType[PORT_1] = TYPE_SENSOR_ULTRASONIC_CONT
         BrickPiSetupSensors()   # Send the properties of sensors to BrickPi
 
+    def eval_position(self):
+        """Returns True if car should keep going straight False otherwise"""
+
+        return
+
     def move(self):
+        BrickPi.MotorSpeed[PORT_A] = self.speed  #Set the speed of MotorA (-255 to 255)
+        BrickPi.MotorSpeed[PORT_B] = self.speed  #Set the speed of MotorB (-255 to 255)
+
+    def drive(self):
         while True:
             print "Running Forward"
-            BrickPi.MotorSpeed[PORT_A] = self.speed  #Set the speed of MotorA (-255 to 255)
-            BrickPi.MotorSpeed[PORT_B] = self.speed  #Set the speed of MotorB (-255 to 255)
-            ot = time.time()
-            while(time.time() - ot < 3):    #running while loop for 3 seconds
-                BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
-                time.sleep(.1)              # sleep for 100 ms
-            print "Running Reverse"
-            BrickPi.MotorSpeed[PORT_A] = -self.speed  #Set the speed of MotorA (-255 to 255)
-            BrickPi.MotorSpeed[PORT_B] = -self.speed  #Set the speed of MotorB (-255 to 255)
-            ot = time.time()
-            while(time.time() - ot < 3):    #running while loop for 3 seconds
-                BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
-                time.sleep(.1)
+            self.move()
+            BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
+            print BrickPi.Sensor[PORT_1]
 
+    def turn(self):
+        """Turns the car 90 degrees"""
+        return
 
 if __name__ == "__main__":
-    car = SelfDrivingCar(100)
-    car.move()
+    car = SelfDrivingCar(255)
+    car.drive()
